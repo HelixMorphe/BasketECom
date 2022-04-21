7b3639f86133a8b008de3b28a8276a25024e6124
Skillcet <skillsntools.web@gmail.com>
2022-04-21 19:33:50 +0530

    login v1; local login added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,13 @@
 import LocalMallIcon from '@mui/icons-material/LocalMall'
+import { useSession, signIn, signOut } from 'next-auth/react'
+// import Login from '../../pages/login'
 function Header() {
+  const { data: session } = useSession()
+
   return (
     <div className="flex flex-row justify-between text-2xl font-normal text-gray-900">
       <div className="text-lg text-gray-900">
-        <div>Hi, Sai!</div>
+        {session && <div>Hi, {session.user.name}</div>}
         <div className="text-base text-gray-500">
           What would you like to buy today?
         </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,32 +1,46 @@
+import { useSession, signIn, signOut } from 'next-auth/react'
+import { useRouter } from 'next/router'
 import Header from '../components/Header'
 import Searchbar from '../components/Searchbar'
 import Banner from '../components/Banner'
 import Categories from '../components/Categories'
 import BestSellers from '../components/BestSellers'
 import Navbar from '../components/Navbar'
+import Login from '../components/Login'
+
 const Home = () => {
-  return (
-    <div>
-      <div className="bg-gray-50 px-4 py-4">
-        <Header />
-        <div className="mt-4">
-          <Searchbar />
+  const { data: session } = useSession()
+  const router = useRouter()
+  if (!session) {
+    return (
+      <div>
+        <Login />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <div className="bg-gray-50 px-4 py-4">
+          <Header />
+          <div className="mt-4">
+            <Searchbar />
+          </div>
+          <div className="mt-4">
+            <Banner />
+          </div>
+          <div className="mt-4">
+            <Categories />
+          </div>
+          <div className="mt-4">
+            <BestSellers />
+          </div>
         </div>
-        <div className="mt-4">
-          <Banner />
-        </div>
-        <div className="mt-4">
-          <Categories />
-        </div>
-        <div className="mt-4">
-          <BestSellers />
+        <div className="sticky bottom-0 bg-gray-50">
+          <Navbar />
         </div>
       </div>
-      <div className="sticky bottom-0 bg-gray-50">
-        <Navbar />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Home
