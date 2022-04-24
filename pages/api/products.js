357580a53e459c3deecef8836ec07fbea2285024
Skillcet <skillsntools.web@gmail.com>
2022-04-24 13:50:01 +0530

    error while hosting

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,5 +1,5 @@
-import dbConnect from '../../../util/dbConnect'
-import Product from '../../../models/Product'
+import dbConnect from '../../util/dbConnect'
+import Product from '../../models/Product'
 
 export default async function handler(req, res) {
   const { method } = req
